23a8d2e4b1c0421ed4613c907a0ea456c93bc58d
Diogo Ferreira <diogo.ferreira@mindera.com>
2019-10-03 18:46:59 +0100

    Add search controls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-    return <div>Hello world!</div>;
+    const [inputValue, setInputValue] = useState('');
+    const [query, setQuery] = useState(null);
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        setQuery(inputValue);
+    };
+
+    return (
+        <div className="app">
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    value={inputValue}
+                    placeholder="please input something"
+                    onChange={event => setInputValue(event.target.value)}
+                ></input>
+                <button type="submit">Search</button>
+            </form>
+            {query && <h1>Results for '{query}'</h1>}
+        </div>
+    );
 }
 
 export default App;
