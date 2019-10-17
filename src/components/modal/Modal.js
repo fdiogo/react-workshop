6798009fb9e5451f6074e50af8be43ab19cfa1a4
Diogo Ferreira <diogo.ferreira@mindera.com>
2019-10-17 18:44:09 +0100

    Add support to close modals on Escape and outside click. Challenge 3

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import './Modal.css';
@@ -8,10 +8,34 @@ import Close from '../icons/Close';
 function Modal(props) {
     const { children, onClose } = props;
 
+    const contentRef = useRef();
+
+    useEffect(() => {
+        const handleKeyUp = event => {
+            if (event.key === 'Escape') {
+                onClose();
+            }
+        };
+
+        const handleMouseDown = event => {
+            if (contentRef.current.contains(event.target) === false) {
+                onClose();
+            }
+        };
+
+        window.addEventListener('mousedown', handleMouseDown);
+        window.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            window.removeEventListener('mousedown', handleMouseDown);
+            window.removeEventListener('keyup', handleKeyUp);
+        };
+    }, [onClose]);
+
     return ReactDOM.createPortal(
         <div className="modal">
             <div className="modal-overlay" />
-            <div className="modal-content">
+            <div className="modal-content" ref={contentRef}>
                 <button className="modal-close" onClick={onClose}>
                     <Close />
                 </button>
